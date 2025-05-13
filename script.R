fcf4b8c9d23a73fc14b36831fa154f2dc878f17f
tomaszaba <tomas.zaba@outlook.com>
2025-05-13 12:57:26 +0200

    update project  analysis workflow

---
+++ script.R
@@ -0,0 +1,21 @@
+################################################################################
+#         FEATURE INSIGHTS ON AFGHANISTAN ACUTE MALNUTRITION ADMISSIONS        #
+#                            ANALYSIS WORKFLOW                                 #
+################################################################################
+
+## ---- Load required libraries ------------------------------------------------
+library(tidyr)
+library(dplyr)
+library(lubridate)
+library(tsibble)
+library(feasts)
+library(ggplot2)
+library(fable)
+library(sf)
+library(cyphr)
+
+## ---- Retrieve secret key ----------------------------------------------------
+secret_key <- data_key(".", Sys.getenv("path_secret_key"))
+
+## ---- Read data in -----------------------------------------------------------
+source("scripts/read-in-data.R")
