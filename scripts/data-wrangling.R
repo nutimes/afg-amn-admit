11296d85198ceda154de69cdad58f4d16bf5eae4
tomaszaba <tomas.zaba@outlook.com>
2025-05-16 13:19:46 +0200

    apply box-cox transformation

--- scripts/data-wrangling.R
+++ scripts/data-wrangling.R
@@ -4,7 +4,7 @@
 
 ## ---- Prepara the data -------------------------------------------------------
 
-admissions <- raw_data |> 
+admissions <- raw_data |>
   select(
     province = Province,
     year = Year,
@@ -12,30 +12,30 @@ admissions <- raw_data |>
     month = Month,
     sam_cases = samAdmittedTotal,
     mam_cases = mamU5
-  ) |> 
+  ) |>
   mutate(
     year = as.numeric(year),
     month = as.numeric(month),
     day = as.numeric(month)
-  ) |> 
+  ) |>
   mutate(
     time = do.call(
       what = make_date,
       args = list(
         year = year,
-        month = month, 
+        month = month,
         day = day
       )
-    ) |> 
+    ) |>
       yearmonth(time),
     .admissions = sam_cases + mam_cases
-) |> 
+  ) |>
   select(
     -c(year, quarter, month, sam_cases, mam_cases, day)
   )
 
 ### ------------------------------------------ Exclude 2012 and 2025's data ----
-admissions <- admissions |> 
+admissions <- admissions |>
   filter(
     time >= yearmonth("2013 Jan") & time <= yearmonth("2024 Dec")
   )
@@ -43,16 +43,33 @@ admissions <- admissions |>
 ### -------------------------------------------------- Summarise admissions ----
 
 ### ------------------------------------------------- At the National level ----
-na <- admissions |> 
+na <- admissions |>
   summarise_admissions(
-    .group = FALSE, 
+    .group = FALSE,
     time = "M"
   )
 
+#### Apply Box-Cox transformation to stabilize variance ----
+lbd_na <- na |> 
+  features(
+    .var = .admissions,
+    features = guerrero
+  ) |> 
+  pull(lambda_guerrero)
+
+#### Transform the data ----
+na_b <- na |> 
+  mutate(
+    .admissions = do.call(
+      what = box_cox,
+      args = list(x = .admissions, lambda = lbd_na)
+    )
+  )
+
 ### ------------------------------------------------ At the Province level -----
-prov <- admissions |> 
+prov <- admissions |>
   summarise_admissions(
-    .group = TRUE, 
+    .group = TRUE,
     time = "M"
   )
 
